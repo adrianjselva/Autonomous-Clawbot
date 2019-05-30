5e01ff968173b6faddd8567986975dbce1ca2495
Adrian Selva <adrianjselva@gmail.com>
2019-05-30 07:11:30 -0500

    Updatated constants definitions

--- include/constants.h
+++ include/constants.h
@@ -18,32 +18,25 @@ const double kDY = 0;
 
 /** Motor Position Values **/
 const int ARM_UP = 500;
-const int ARM_DOWN = 0;
+const int ARM_DOWN = 7;
 const int CLAW_OPEN = 5;
 const int CLAW_CLOSED = 230;
 
 /** Vision dimensions **/
-const int visionWidth = 316;
-const int visionHeight = 212;
+const int VISION_VIEW_WIDTH = 316;
+const int VISION_VIEW_HEIGHT = 212;
 
 /** Screen Offset **/
-const int xOffset = 82;
-const int xVisionOffset = (xOffset + visionWidth);
-const int leftBound = xOffset + MIN_X;
-const int rightBound = xOffset + MAX_X;
-const int middleBound = xOffset + MID_X;
-const int displayHeight = 50;
+const int LEFT_BOUND_OFFSETTED = 82;
+const int RIGHT_BOUND_OFFSETTED = (LEFT_BOUND_OFFSETTED + VISION_VIEW_WIDTH);
+const int LEFT_X_CTR = LEFT_BOUND_OFFSETTED + MIN_X;
+const int RIGHT_X_CTR = LEFT_BOUND_OFFSETTED + MAX_X;
+const int MIDDLE_X_CTR = LEFT_BOUND_OFFSETTED + MID_X;
 
-/** Screen drawing variables **/
-const int circleRadius = 2;
-const int textOffset = 4;
+const int DISPLAY_HEIGHT = 50;
 
-/** Distance Bounds **/
-const int distanceUpper = 200;
-const int distanceLower = 190;
-
-const int redDistanceUpper = 190;
-const int redDistanceLower = 180;
+/** Size of circles **/
+const int CIRCLE_RADIUS = 2;
 
 /** Status Constants **/
 const int STATUS_TRACK = 0;
@@ -66,49 +59,58 @@ const int CLAW_OPEN_STATUS = 1;
 const int XDIRECTION = 0;
 const int YDIRECTION = 1;
 
-/** **/
+/** Object type ID **/
 const int UNKNOWN_OBJECT = 0;
 const int YELLOW_OBJECT = 1;
 const int RED_OBJECT = 2;
 const int RED_YELLOW_OBJECT = 3;
 
+/** Views **/
 const int ACTION_V = 0;
 const int VISION_V = 1;
 
+/** Vision Map Button Bounds **/
 const int VISION_MAP_BUTTON_LOWER_BOUND_X = 295;
 const int VISION_MAP_BUTTON_UPPER_BOUND_X = 405;
 const int VISION_MAP_BUTTON_LOWER_BOUND_Y = 173;
 const int VISION_MAP_BUTTON_UPPER_BOUND_Y = 203;
 
-const int STOP_BUTTON_LOWER_BOUND_X = xVisionOffset + 21;
-const int STOP_BUTTON_UPPER_BOUND_X = xVisionOffset + 61;
-const int STOP_BUTTON_LOWER_BOUND_Y = 10;
-const int STOP_BUTTON_UPPER_BOUND_Y = 50;
-
-const int EXIT_BUTTON_LOWER_BOUND_X = xVisionOffset + 21;
-const int EXIT_BUTTON_UPPER_BOUND_X = xVisionOffset + 61;
-const int EXIT_BUTTON_LOWER_BOUND_Y = 10;
-const int EXIT_BUTTON_UPPER_BOUND_Y = 50;
-
-const int EXIT_BUTTON_X = xVisionOffset + 21;
-const int EXIT_BUTTON_Y = 10;
-const int EXIT_BUTTON_WIDTH = 40;
-const int EXIT_BUTTON_HEIGHT = 40;
-
-const int EXIT_TEXT_X = xVisionOffset + 25;
-const int EXIT_TEXT_Y = 35;
-
-const int STOP_TEXT_X = xVisionOffset + 25;
-const int STOP_TEXT_Y = 35;
-
-const int STOP_BUTTON_X = xVisionOffset + 21;
-const int STOP_BUTTON_Y = 10;
-const int STOP_BUTTON_WIDTH = 40;
-const int STOP_BUTTON_HEIGHT = 40;
-
+/** Vision map button position and size **/
 const int VISION_BUTTON_X = 295;
 const int VISION_BUTTON_Y = 173;
 const int VISION_BUTTON_WIDTH = 110;
 const int VISION_BUTTON_HEIGHT = 30;
+
+/** Stop Button Bounds **/
+const int STOP_BUTTON_LOWER_BOUND_X = RIGHT_BOUND_OFFSETTED + 21;
+const int STOP_BUTTON_UPPER_BOUND_X = RIGHT_BOUND_OFFSETTED + 61;
+const int STOP_BUTTON_LOWER_BOUND_Y = 10;
+const int STOP_BUTTON_UPPER_BOUND_Y = 50;
+
+/** Exit button bounds **/
+const int EXIT_BUTTON_LOWER_BOUND_X = RIGHT_BOUND_OFFSETTED + 21;
+const int EXIT_BUTTON_UPPER_BOUND_X = RIGHT_BOUND_OFFSETTED + 61;
+const int EXIT_BUTTON_LOWER_BOUND_Y = 10;
+const int EXIT_BUTTON_UPPER_BOUND_Y = 50;
+
+/** Exit button position and size **/
+const int EXIT_BUTTON_X = RIGHT_BOUND_OFFSETTED + 21;
+const int EXIT_BUTTON_Y = 10;
+const int EXIT_BUTTON_WIDTH = 40;
+const int EXIT_BUTTON_HEIGHT = 40;
+
+/** Exit text position **/
+const int EXIT_TEXT_X = RIGHT_BOUND_OFFSETTED + 25;
+const int EXIT_TEXT_Y = 35;
+
+/** Stop text position **/
+const int STOP_TEXT_X = RIGHT_BOUND_OFFSETTED + 25;
+const int STOP_TEXT_Y = 35;
+
+/** Stop button position and size **/
+const int STOP_BUTTON_X = RIGHT_BOUND_OFFSETTED + 21;
+const int STOP_BUTTON_Y = 10;
+const int STOP_BUTTON_WIDTH = 40;
+const int STOP_BUTTON_HEIGHT = 40;
 
 #endif
